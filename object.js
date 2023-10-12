26869c1ae673d72dc25ebd7387ecb0f8b4fdb50f
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2023-10-12 09:07:42 -0400

    g

--- object.js
+++ object.js
@@ -4,24 +4,52 @@ const character =
 [
    {
     Name: "Master Chief",
-    Primaryweapon: "Assault Rifle",
+    Primaryweapon: "Magnum",
     Race: "Human",
+    Alive: true,
+    appearance: [1, 2, 3, 4, 5],
    },
    {
     Name: "Arbiter",
     Primaryweapon: "Energy Sword",
     Race: "Sangheili",
+    Alive: true,
+    appearance: [2, 3, 5],
    },
    {
     Name: "Didact",
     Primaryweapon: "Unknown",
     Race: "Forerunner",
+    Alive: true,
+    appearance: [4, 5],
+   },
+   {
+    Name: "Sergeant Johnson",
+    Primaryweapon: "Assualt Rifle",
+    Race: "Human",
+    Alive: false,
+    appearance: [1, 2, 3],
    },
 ];
 
-character.forEach((character) => console.log(character.Name));
+//character.forEach((character) => console.log(character.Name));
 
-console.log(character);
+
+//
+
+//character.forEach((character) => console.log(character.appearance));
+character.forEach( function (hi) {
+    for (var i = 0; i < character.appearance.length; i++) {
+        console.log(appearance.length)
+    }
+})
+
+/*for (var i = 0; i < character.appearance.length; i++) {
+    console.log()
+}*/
+
+//
+
 
 //const graduates = students.filter((student) => student.graduated === true);
 
