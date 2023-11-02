d493404204715f4e7ded0f732ae0f027f6f5d0f7
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2023-11-02 09:24:38 -0400

    df

--- for.js
+++ for.js
@@ -7,7 +7,7 @@ while(money < 7) {
     money--;
 } */
 
-function linearSearch(num, arr) {
+/*function linearSearch(num, arr) {
     let found;
     for(let i=0; i < arr.length; i++) {
         if(arr[i] === num) {
@@ -27,5 +27,28 @@ function linearSearch(num, arr) {
     return found;
 }
 
-linearSearch(50, [1, 2, 4, 5, 6, 7]);
+linearSearch(50, [1, 2, 4, 5, 6, 7]);*/
 
+
+
+const c3mp = ["A", "B", "C", "D", "E"];
+
+function btn1(arr, firstnum, lastnum) {
+    arr[firstnum] = arr.splice(lastnum, 1, arr[firstnum]) [0]; //prevents E from turning into own array
+    console.log(arr); //for test 
+}
+
+function btn2(arr, lastnum) {
+    arr.unshift(arr[lastnum]);
+    arr.splice(5, 1);
+    console.log(arr);
+}
+
+function btn3(arr, firstnum, secondnum) {
+    arr[firstnum] = arr.splice(secondnum, 1, arr[firstnum]) [0];
+    console.log(arr); //for test
+}
+
+//btn1(c3mp, 0, 4);
+btn2(c3mp, 4)
+//btn3(c3mp, 0, 1);
